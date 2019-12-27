b0cc3aaa93c9426084a4a306415fb4a6709607db
zenja90907 <55218478+zenja90907@users.noreply.github.com>
2019-12-27 11:12:20 +0800

    期末(完成player)

---
+++ Unity_AI2D_B1040445_Eason/Assets/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int speed = 50;
+    public float jump = 5f;
+    private bool isGround;
+    private Rigidbody2D r2d;
+    private Transform tra;
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        tra = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) Turn();
+        if(Input.GetKeyDown(KeyCode.A)) Turn(180);
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+    }
+
+    /// <summary>
+    /// 走路
+    /// </summary>
+    private void Walk()
+    {
+        r2d.AddForce(new Vector2(speed * Input.GetAxis("Horizontal"),0));
+    }
+
+    /// <summary>
+    /// 跳躍
+    /// </summary>
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&&isGround==true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jump));
+        }
+    }
+
+    /// <summary>
+    /// 轉彎
+    /// </summary>
+    /// <param name="direction"></param>
+    private void Turn(int direction=0)
+    {
+        tra.eulerAngles = new Vector3(0, direction,0);
+    }
+}
